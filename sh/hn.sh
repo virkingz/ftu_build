81488bdd52a0863bc3421b767f3367c5dfdf8658
virkingz <68280845+virkingz@users.noreply.github.com>
2024-04-25 16:30:43 +0800

    Update hn.sh

--- sh/hn.sh
+++ sh/hn.sh
@@ -4,6 +4,7 @@ git clone --depth=1 git@e.coding.net:yu308/project/STM32CubeF4.git stm32hal
 cp stm32hal/Drivers/CMSIS/Lib/GCC/libarm_cortexM4lf_math.a stm32hal/
 git clone git@e.coding.net:yu308/project/rtthread-nano.git rtthread
 git clone https://gitee.com/RT-Thread-Mirror/uffs.git pkgs/uffs
+git clone git@e.coding.net:yu308/project/cJSON.git pkgs/cjson
 
 #修改 uffs
 sed -i "s/st_atime/atime/g" pkgs/uffs/src/uffs/uffs_fd.c
