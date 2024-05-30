b8b05409acc30d330155b181d57191f221b7edaa
virkingz <68280845+virkingz@users.noreply.github.com>
2024-05-30 13:13:23 +0800

    Update fjauto.sh

--- sh/fjauto.sh
+++ sh/fjauto.sh
@@ -10,15 +10,18 @@ git submodule sync
 # 获取子模块
 git submodule update --init --recursive
 
+# 同步子模块远程仓库至本地
+git submodule update --remote
+
 #rtthread
-git clone --recursive -b v5.0.2 https://github.com/RT-Thread/rt-thread.git rtthread
+git clone --recursive -b v4.0.5 https://github.com/RT-Thread/rt-thread.git rtthread
 #
 git clone --recursive -b v1.9.1 git@e.coding.net:yu308/project/STM32CubeH7.git stm32hal
 cp stm32hal/Drivers/CMSIS/DSP/Lib/GCC/libarm_cortexM7lfdp_math.a stm32hal/
 
-#uffs
-git clone https://gitee.com/RT-Thread-Mirror/uffs.git pkgs/uffs
-git clone https://gitee.com/RT-Thread-Mirror/CmBacktrace.git pkgs/cmbacktrace
+
+# 修改 stdio 注释第67行
+sed -i -e '67 s/^/\/\//' rtthread/components/libc/compilers/gcc/newlib/stdio.c
 
 # 修改 uffs
 sed -i "s/st_atime/atime/g" pkgs/uffs/src/uffs/uffs_fd.c
@@ -29,13 +32,4 @@ sed -i "s/st_atime/atime/g" pkgs/uffs/src/inc/uffs/uffs_fd.h
 sed -i "s/st_mtime/mtime/g" pkgs/uffs/src/inc/uffs/uffs_fd.h
 sed -i "s/st_ctime/ctime/g" pkgs/uffs/src/inc/uffs/uffs_fd.h
 
-# 修改 wiznet
-sed -i 's/RT_WEAK/rt_weak/g' pkgs/wiznet/src/wiz.c
-sed -i 's/dfs_fd/dfs_file/g' pkgs/wiznet/src/wiz_af_inet.c
-
-
-# 修改 cmbacktrace
-sed -i 's/RT_WEAK/rt_weak/g' pkgs/cmbacktrace/cmb_port.c
-
-# 修改 RTT ctime.c
-sed -i '1,751d' rtthread/components/libc/compilers/common/ctime.c
+sed -i '20i #include <string.h>' rtthread/components/drivers/include/drivers/mtd_nand.h
